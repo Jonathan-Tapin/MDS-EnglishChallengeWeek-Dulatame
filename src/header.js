4e4bc44a52ca116fd7c5232c39b5d464f30b62fe
Jonathan Tapin <jonathan.tapin@gmail.com>
2020-12-02 11:53:21 +0100

    debut refonte home

--- src/header.js
+++ src/header.js
@@ -14,7 +14,6 @@ function Header() {
   return (
     <div>
       <Navbar bg="light transparent" expand="lg">
-        <Navbar.Brand href="/">Santa Market</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse
           className="justify-content-center"
@@ -26,16 +25,13 @@ function Header() {
             </Nav.Link>
               <NavDropdown title="Artisan" id="basic-nav-dropdown">
             {Artisant.map((dat) => (
-                <NavDropdown.Item href={"/catalog?id="+ dat.id}>
+              <NavDropdown.Item href={"/catalog?id="+ dat.id}>
                   {dat.title}
                 </NavDropdown.Item>
             ))}
               </NavDropdown>
           </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Search</Button>
-          </Form>
+            <Navbar.Brand href="/">Santa Market</Navbar.Brand>
           <Nav>
             <Nav.Link href="/cart">Cart</Nav.Link>
           </Nav>
