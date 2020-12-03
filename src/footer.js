49813053c07d522898df6198561389d391ba0529
Jonathan Tapin <jonathan.tapin@gmail.com>
2020-12-03 10:15:36 +0100

    footer terminé

--- src/footer.js
+++ src/footer.js
@@ -7,16 +7,32 @@ import {
   faInstagram,
   faTwitter,
 } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelopeOpenText} from "@fortawesome/free-solid-svg-icons";
+
+import { Container, Row, Col, Form } from "react-bootstrap";
+
 
 function Footer() {
   return (
-      <div >
-          
-          <div className="newsletter">
-              
-        </div>
-          
-
+    <div>
+      <div className="newsletter">
+        <Container>
+          <Row>
+            <Col className="email-col">
+              <h2>Santa's letter</h2>
+              <p>Please enter your email adress to receive news about products, caftsmen's news and inspiration gift's list</p>
+              <Form.Group as={Row} controlId="formHorizontalEmail">
+                <Col sm={10}>
+                  <Form.Control type="email" placeholder="your.email@mail.com" />
+                </Col>
+              </Form.Group>
+            </Col>
+            <Col className="icon">
+              <FontAwesomeIcon className="newsletter-icon" icon={ faEnvelopeOpenText } size="9x"/>
+            </Col>
+          </Row>
+        </Container>
+      </div>
 
       <div className="footer">
         <div className="logo-footer">
